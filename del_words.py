f47c1f8c995143cd48ebba9aa7c3577bddafd84f
annkatsko <1187484@gmail.com>
2022-06-02 21:20:29 +0300

    last commit 02/06 12.23

--- del_words.py
+++ del_words.py
@@ -1,23 +1,29 @@
-def open_and_read_some_file(some_file_name: str, designation='+') -> list:
-    """this function opens and reads file, returns list of words from file's text"""
-    list_of_words = open(some_file_name).read().split()
-    return list_of_words
+import re
+def read_some_file(some_file_name: str) -> list:
+    """this function reads file, returns list of lines from file's text"""
+    data = open(some_file_name).readlines()
+    return data
 
 
-def filter_out_words(file1: list, file2: list) -> list:
-    """this function removes file2's words from file1 if they exist in file1 """
-    for i in file2:
-        if i in file1:
-            file1.remove(i)
-    return file1
+def filter_out_words(data: list, bad_words_file: str) -> str:
+    """this function removes file2's words from file1 if they exist in file1"""
+    bad_words = open(bad_words_file).read().split()
+    new_data = []
+    for line in data:
+        for word in bad_words:
+            if word in line:
+                line = line.replace(word, '')
+        new_data.append(line)
+    return ' '.join(new_data)
 
 
-def del_words(name_filе1: str, name_filе2: str, new_filе_name: str):
-    """this function removes file2's words from file1 if they exist in file1, and writes text into a new file"""
-    list_of_words_from_file1 = open_and_read_some_file(name_filе1)
-    list_of_words_from_file2 = open_and_read_some_file(name_filе2)
+def write_text_into_file(data: list, bad_words_file: str, new_filе_name: str) -> None:
+    """this function writes text into a new file"""
     with open(new_filе_name, 'w') as opened_new_file:
-        opened_new_file.write(' '.join(filter_out_words(list_of_words_from_file1, list_of_words_from_file2)))
+        opened_new_file.write(filter_out_words(data, bad_words_file))
 
 
-del_words('text.txt', ' bad_words.txt', 'new_sorted_file')
+write_text_into_file(read_some_file('text.txt'), ' bad_words.txt', 'new_sorted_file')
+
+
+
