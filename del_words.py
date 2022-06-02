2d5a0e6d610aaa2246b79c9769305e023ba72227
annkatsko <1187484@gmail.com>
2022-06-02 22:06:31 +0300

    last commit 02/06 21.22

--- del_words.py
+++ del_words.py
@@ -1,29 +1,33 @@
-import re
-def read_some_file(some_file_name: str) -> list:
-    """this function reads file, returns list of lines from file's text"""
-    data = open(some_file_name).readlines()
-    return data
-
-
-def filter_out_words(data: list, bad_words_file: str) -> str:
-    """this function removes file2's words from file1 if they exist in file1"""
-    bad_words = open(bad_words_file).read().split()
+def filter_out_words(data: list, bad_words: list) -> list:
+    """Function remove all words from list of bad_words"""
     new_data = []
     for line in data:
         for word in bad_words:
             if word in line:
                 line = line.replace(word, '')
         new_data.append(line)
-    return ''.join(new_data)
+    return new_data
 
 
-def write_text_into_file(data: list, bad_words_file: str, new_filе_name: str) -> None:
-    """this function writes text into a new file"""
-    with open(new_filе_name, 'w') as opened_new_file:
-        opened_new_file.write(filter_out_words(data, bad_words_file))
+def get_data_from_file(filepath: str) -> list:
+    """Convert data from file to list"""
+    with open(filepath, encoding="utf-8") as file:
+        return list(file.readlines())
 
 
-write_text_into_file(read_some_file('text.txt'), ' bad_words.txt', 'new_sorted_file')
+def write_data(filepath: str, data: list) -> None:
+    """Write data into the file"""
+    with open(filepath, "w", encoding="utf-8") as file:
+        file.writelines(data)
 
 
+def main():
+    """Main function"""
+    data = get_data_from_file("text.txt")
+    words = get_data_from_file("bad_words.txt")
+    filtered_data = filter_out_words(data, words)
+    write_data("filtered_text.txt", filtered_data)
 
+
+if __name__ == '__main__':
+    main()
