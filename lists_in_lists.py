a1212ffc0758288cba2b29a1441ab00e26d0271c
annkatsko <1187484@gmail.com>
2022-06-10 09:18:04 +0300

    last commit 10.06 9.17

--- lists_in_lists.py
+++ lists_in_lists.py
@@ -1,31 +1,25 @@
-lst1 = [[1, 76, 2,  99, 99, 99],
-        [1, 99, 99, 99, 76, 2, 52, 23, 22, 77],
-        [1, 99, 10, 17, 49, 35, 66, 14],
-        [1, 99, 99, 2],
-        [1, 99, 99, 99, 99, 2]]
-common_elements = []
-for element in lst1[0]:
-    if element in common_elements:
-        continue
-    for element2 in lst1[1]:
-        if element == element2:
-            common_elements.append(element)
-print(common_elements)
-for index in range(2, len(lst1)):
-    for elem in common_elements:
-        if elem in lst1[index]:
-            continue
+def find_common_elements(list1: list, list2: list) -> list:
+    """Function removes not common elements of lists from list1"""
+    for element in list1:
+        n = 1
+        if element in list2:
+            if list1.count(element) >= list2.count(element):
+                while list1.count(element) - list2.count(element) >= n:
+                    list1.remove(element)
+                    n += 1
         else:
-            common_elements.remove(elem)
+            list1.remove(element)
+    return list1
+
+
+lst1 = [[1, 76, 99, 4, 99, 99],
+        [1, 99, 99, 4, 76, 2, 52, 23, 22, 77],
+        [99, 99, 17, 4, 2, 76, 66, 14],
+        [1, 76, 99, 2, 4],
+        [1, 76, 2, 4, 99]]
+index = 2
+common_elements = find_common_elements(lst1[0], lst1[1])
+while index < len(lst1):
+    common_elements = find_common_elements(common_elements, lst1[index])
+    index += 1
 print(common_elements)
-
-
-
-
-
-
-
-
-
-
-
